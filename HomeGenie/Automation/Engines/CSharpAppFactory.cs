db7ac14219d79e29fdf86b12fbfe286aab620c3c
Matthieu Tudury <matthieu.tudury@gmail.com>
2019-07-27 21:33:00 +0200

    for some advanced usages allow raw scripts

--- HomeGenie/Automation/Engines/CSharpAppFactory.cs
+++ HomeGenie/Automation/Engines/CSharpAppFactory.cs
@@ -231,24 +231,40 @@ namespace HomeGenie.Automation.Scripting
             compilerParams.ReferencedAssemblies.Add("NLog.dll");
             compilerParams.ReferencedAssemblies.Add("Newtonsoft.Json.dll");
 
-            compilerParams.ReferencedAssemblies.Add("SerialPortLib.dll");
-            compilerParams.ReferencedAssemblies.Add("NetClientLib.dll");
+            if (scriptSetup.StartsWith("//@rawcsharpscript")) {
+                var scriptSetupReferences = scriptSetup.Replace("\r", "").Split('\n');
 
-            compilerParams.ReferencedAssemblies.Add("UPnP.dll");
+                foreach(var reference in scriptSetupReferences)
+                {
+                    if (reference.StartsWith("Reference="))
+                    compilerParams.ReferencedAssemblies.Add(reference.Substring(10));
+                }
 
-            //if (Raspberry.Board.Current.IsRaspberryPi)
+                source = scriptSource;
+            }
+            else
             {
-                compilerParams.ReferencedAssemblies.Add("Raspberry.IO.dll");
-                compilerParams.ReferencedAssemblies.Add("Raspberry.IO.Components.dll");
-                compilerParams.ReferencedAssemblies.Add("Raspberry.IO.GeneralPurpose.dll");
-                compilerParams.ReferencedAssemblies.Add("Raspberry.IO.InterIntegratedCircuit.dll");
-                compilerParams.ReferencedAssemblies.Add("Raspberry.IO.SerialPeripheralInterface.dll");
-                compilerParams.ReferencedAssemblies.Add("Raspberry.System.dll");
-                compilerParams.ReferencedAssemblies.Add("UnitsNet.dll");
+                compilerParams.ReferencedAssemblies.Add("SerialPortLib.dll");
+                compilerParams.ReferencedAssemblies.Add("NetClientLib.dll");
+
+                compilerParams.ReferencedAssemblies.Add("UPnP.dll");
+
+                //if (Raspberry.Board.Current.IsRaspberryPi)
+                {
+                    compilerParams.ReferencedAssemblies.Add("Raspberry.IO.dll");
+                    compilerParams.ReferencedAssemblies.Add("Raspberry.IO.Components.dll");
+                    compilerParams.ReferencedAssemblies.Add("Raspberry.IO.GeneralPurpose.dll");
+                    compilerParams.ReferencedAssemblies.Add("Raspberry.IO.InterIntegratedCircuit.dll");
+                    compilerParams.ReferencedAssemblies.Add("Raspberry.IO.SerialPeripheralInterface.dll");
+                    compilerParams.ReferencedAssemblies.Add("Raspberry.System.dll");
+                    compilerParams.ReferencedAssemblies.Add("UnitsNet.dll");
+                }
+
+                compilerParams.ReferencedAssemblies.Add(Path.Combine("lib", "shared", "Innovative.Geometry.dll"));
+                compilerParams.ReferencedAssemblies.Add(Path.Combine("lib", "shared", "Innovative.SolarCalculator.dll"));
+
             }
 
-            compilerParams.ReferencedAssemblies.Add(Path.Combine("lib", "shared", "Innovative.Geometry.dll"));
-            compilerParams.ReferencedAssemblies.Add(Path.Combine("lib", "shared", "Innovative.SolarCalculator.dll"));
 
             // compile and generate script assembly
             return provider.CompileAssemblyFromSource(compilerParams, source);
